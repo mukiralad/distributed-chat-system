410ef95d99badbc1956a9ffbebb0ff6d118d8407
mukiralad <67241568+mukiralad@users.noreply.github.com>
2025-02-13 09:07:34 -0500

    Added basic unit tests for server and client

---
+++ tests/test_client.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestClient(unittest.TestCase):
+    def test_sample(self):
+        self.assertTrue("chat" in "distributed chat system")
+
+if __name__ == "__main__":
+    unittest.main()
